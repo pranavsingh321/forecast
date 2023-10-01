fb3bd01d4c5ba1120f22e79f482dd4ea17e9a268
pranavsingh321 <pranavsingh321@gmail.com>
2023-10-01 17:24:14 +0200

    rust api example

---
+++ init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS cities (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	lat NUMERIC NOT NULL,
+	long NUMERIC NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS cities_name_idx ON cities (name);
+
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
